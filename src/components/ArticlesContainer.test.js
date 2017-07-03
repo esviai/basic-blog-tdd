73ec96280ebf8a53bb1103e6d5a136a0f235fdf0
ingelieur <ingelieur@gmail.com>
2017-07-03 15:30:23 +0700

    ArticlesContainer test

--- src/components/ArticlesContainer.test.js
+++ src/components/ArticlesContainer.test.js
@@ -12,4 +12,15 @@ describe('ArticlesContainer component', () => {
     const articlesContainerWrapper = shallow(<ArticlesContainer />)
     expect(articlesContainerWrapper.find('Article')).toHaveLength(1)
   })
+  it('has articles state', () => {
+    const articlesContainerWrapper = shallow(<ArticlesContainer />)
+    articlesState = articlesContainerWrapper.state('articles')
+    expect(articlesState).toBeDefined()
+  })
+  it('has article as an object', () => {
+    const articlesContainerWrapper = shallow(<ArticlesContainer />)
+    articlesState = articlesContainerWrapper.state('articles')
+    isArticleAnObject = typeof  articlesState[0] === 'object'
+    expect(isArticleAnObject).toBeTruthy()
+  })
 })
